27bc22536843d9116ed36e8b6233857bbd1449f9
Дмитрий Ордин <dmitrijordin@MacBook-Pro-Dmitrij.local>
2022-11-25 13:34:51 +0300

    feat(deploy): add settings

--- public/user.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.randomName = void 0;
-const faker_1 = require("@faker-js/faker");
-exports.randomName = faker_1.faker.name.fullName();
-//# sourceMappingURL=user.js.map
--- src/user.ts
+++
@@ -1,3 +0,0 @@
-import { faker } from '@faker-js/faker'
-
-export const  randomName = faker.name.fullName()
