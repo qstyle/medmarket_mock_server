5f6fb4239d38c0ea562b2da9a2ab1e1777d0b11d
Дмитрий Ордин <dmitrijordin@MacBook-Pro-Dmitrij.local>
2022-11-25 13:20:24 +0300

    fix(prod): fix env

---
+++ public/user.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.randomName = void 0;
+const faker_1 = require("@faker-js/faker");
+exports.randomName = faker_1.faker.name.fullName();
+//# sourceMappingURL=user.js.map
