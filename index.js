ba0fdb4bc8b4517e8d5504460f9a3d6d59608b18
Дмитрий Ордин <dmitrijordin@MacBook-Pro-Dmitrij.local>
2022-11-28 11:46:14 +0300

    feat(banner): add banner field

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
-// Import packages
 const express = require("express");
 const user = require("./routes/user");
+const mainPageBanner = require("./routes/mainPageBanner");
 
-// Middlewares
 const app = express();
 app.use(express.json());
 
 
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
+app.use(function (_, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-  // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
   next();
 });
 
-// Routes
 app.use("/user", user);
+app.use("/mainPageBanner", mainPageBanner);
 
-// connection
-const port = process.env.PORT || 9001;
+const port  = 9001 ;
 app.listen(port);
--- routes/mainPageBanner.js
+++ routes/mainPageBanner.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { faker } = require('@faker-js/faker')
 
 router.get("/", async (_, res) => {
   return res.status(200).json({
-    name: faker.name.fullName(),
-    message: "The app is working properly!",
+    title: 'Title Banner228' ,
+    titleDesc: "Title Banner Description!",
+    description: 'Banner description Banner description Banner description Banner description',
   });
 });
 
